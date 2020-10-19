3f766e0ce3444b5ede116c43f4587463a23d217d
Cody White <51343804+ecodyswhite@users.noreply.github.com>
2020-10-19 11:48:41 -0700

    Add files via upload

---
+++ soundplayer.js
@@ -0,0 +1,12 @@
+let x = document.getElementById("audio")
+let y = document.getElementById("audio2")
+let z = document.getElementById("audio3")
+let a = document.getElementById("audio4")
+
+function playAudio(x) {
+    x.play()
+}
+
+function pauseAudio(x) {
+    x.pause()
+}
